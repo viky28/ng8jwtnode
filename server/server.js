6c58c9452ff7d386328f2980804cdece6fd65a1e
viky28 <viky.pu28@gmail.com>
2020-03-09 14:18:18 +0530

    first commit

---
+++ server/server.js
@@ -0,0 +1,46 @@
+const express = require('express')
+const app = express()
+const bodyParser = require('body-parser')
+const cors = require('cors')
+
+var corsOptions = {
+    origin : "http://localhost:8081"
+}
+require("./routes/auth.routes")(app);
+require("./routes/user.routes")(app);
+const db = require("./models");
+const Role = db.role;
+db.sequelize.sync({force: true}).then(() => {
+  console.log('Drop and Resync Db');
+  initial();
+});
+app.use(cors(corsOptions))
+// parse requests of content-type - application/json
+app.use(bodyParser.json())
+// parse requests of content-type - application/x-www-form-urlencoded
+app.use(bodyParser.urlencoded({extended:true}))
+// simple route
+app.get('/', (req,res)=>{
+    res.json({message: "Welcome to Viky's application"})
+})
+const PORT = process.env.PORT || 8080
+app.listen(PORT, () => {
+    console.log(`Server is running on port ${PORT}.`)
+})
+
+function initial() {
+    Role.create({
+      id: 1,
+      name: "user"
+    });
+   
+    Role.create({
+      id: 2,
+      name: "moderator"
+    });
+   
+    Role.create({
+      id: 3,
+      name: "admin"
+    });
+  }
